bf3cb369130e5ae4526ad318f5661b3f6a45a8a6
4rshiq <4rshiq007@gmail.com>
2026-09-17 18:12:36 +0530

    add questions

--- week3/char.cpp
+++ week3/char.cpp
@@ -1,6 +1,19 @@
 // 5) character classification
 #include<iostream>
+using namespace std;
 int main()
 {
+    char input;
+    cout<<"enter a character of your choice: ";
+    cin>>input;
+    switch (input)
+    {
+    case :
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
     return 0;
 }
