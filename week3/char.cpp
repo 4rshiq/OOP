530cea35c519b4a1603aa82b82a1cb060e4d61e2
4rshiq <4rshiq007@gmail.com>
2026-09-19 14:19:11 +0530

    implement code

--- week3/char.cpp
+++ week3/char.cpp
@@ -1,19 +1,60 @@
 // 5) character classification
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main()
 {
-    char input;
-    cout<<"enter a character of your choice: ";
-    cin>>input;
-    switch (input)
+char ch;
+
+cout << "Enter a character: ";
+cin >> ch;
+
+switch (ch)
+{
+
+case 'a':
+case 'e':
+case 'i':
+case 'o':
+case 'u':
+    cout << "Vowel";
+    break;
+
+case 'A':
+case 'E':
+case 'I':
+case 'O':
+case 'U':
+    cout << "Vowel";
+    break;
+
+
+case '0':
+case '1':
+case '2':
+case '3':
+case '4':
+case '5':
+case '6':
+case '7':
+case '8':
+case '9':
+    cout << "Digit";
+    break;
+
+default:
+    // We need another check to distinguish
+    // consonants from special characters.
+    if ((ch >= 'a' && ch <= 'z') ||
+        (ch >= 'A' && ch <= 'Z'))
     {
-    case :
-        /* code */
-        break;
-    
-    default:
-        break;
+        cout << "Consonant";
     }
-    return 0;
+    else
+    {
+        cout << "Special character";
+    }
+}
+
+return 0;
 }
